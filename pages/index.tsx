2cc344c40f2900250c49fc555afb3689de7abd14
Diego Fernandez <dfgind@gmail.com>
2023-12-14 20:44:29 +0100

    fisrt

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Background from "../components/Background"
+import Section from "../components/Section"
+import Section2 from "../components/Section2"
+import Section3 from "../components/Section3"
+
+
+function HomePage() {
+    return (
+        <div>
+            <div id="container">
+                <Section />
+                <Section2 />
+                <Section3 />
+            </div>
+
+            <Background />
+        </div>
+    )
+}
+
+
+export default HomePage
